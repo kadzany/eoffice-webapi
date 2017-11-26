0b78f5d77ad1d0ff2d1367ac89285f6d7c4eff5d
Khaerul Adzany <khaerul.adzany@gmail.com>
2017-11-26 16:11:17 +0700

    copy legacy models

---
+++ application/controllers/auth.php
@@ -0,0 +1,13 @@
+<?php
+require(APPPATH . 'libraries/REST_Controller.php');
+
+class Auth extends REST_Controller
+{
+    public function index_get()
+    {
+    }
+
+    public function index_post()
+    {
+    }
+}
---
+++ application/models/legacy/gaji_model.php
@@ -0,0 +1,4 @@
+<?php
+
+class Gaji_model extends CI_Model {
+}
